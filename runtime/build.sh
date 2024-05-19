e6386033ec40e30e0e6023ca057227e562442319
Daniel Mandea <daniel.mandea@yahoo.com>
2024-05-19 17:24:23 +0300

    Removed unnecessary steps under build.

--- runtime/build.sh
+++ runtime/build.sh
@@ -3,15 +3,6 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-echo "Installing dependencies..."
-npm install
-
-echo "Running lint..."
-npm run lint
-
-echo "Running tests..."
-npm test
-
 echo "Cleaning up..."
 rm -rf node_modules
 
